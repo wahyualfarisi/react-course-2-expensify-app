b4ce01a114ebcaa5f82635f1fea5d3f9009222bf
wahyu alfarisi <wahyualfarisi@gmail.com>
2018-09-21 15:49:09 +0700

    change app.js

--- src/app.js
+++ src/app.js
@@ -5,24 +5,11 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore'; 
 
-import { addExpense } from './actions/expenses';
-import { sortByDate } from './actions/filters';
-import getVisibleExpenses from './reducers/expenses';
-
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
 
 const store = configureStore();
-store.dispatch(sortByDate());
-store.dispatch(addExpense({description: 'water bill', amount: 4500}))
-store.dispatch(addExpense({description: 'gas bill', createdAt: 300}))
-store.dispatch(addExpense({description: 'Rent Bill', amount: 109500}))
-
-
-
-const state = store.getState();
-const visibleExpense = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpense);
 
 const jsx = (
